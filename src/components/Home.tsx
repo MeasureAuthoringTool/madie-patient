ef11808bbb4b9384bcc2b409fe5214b032863bc4
Rohit Kandimalla <rohit.kandimalla@semanticbits.com>
2021-12-30 10:16:27 -0500

    Exported madie-patient home component

---
+++ src/components/Home.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Home() {
+  return <div>Patient work in progress !!</div>;
+}
--- src/madie-madie-patient.tsx
+++ src/madie-madie-patient.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { FC } from "react";
 import ReactDOM from "react-dom";
 import singleSpaReact from "single-spa-react";
 import Root from "./root.component";
+import Home from "./components/Home";
 
 const lifecycles = singleSpaReact({
   React,
@@ -12,5 +13,7 @@ const lifecycles = singleSpaReact({
     return null;
   },
 });
+
+export const MadiePatient: FC = Home;
 
 export const { bootstrap, mount, unmount } = lifecycles;
