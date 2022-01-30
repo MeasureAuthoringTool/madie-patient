c737104e8236d5a29b99a1226948a5f8bff8e823
Joseph Kotanchik <joseph.kotanchik@semanticbits.com>
2022-01-30 15:05:38 -0500

    Import json-mode to enable syntax highlighting in react-ace

--- src/components/editor/Editor.tsx
+++ src/components/editor/Editor.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import AceEditor from "react-ace";
 import { Ace } from "ace-builds";
+import "ace-builds/src-noconflict/mode-json";
 
 export interface EditorPropsType {
   value: string;
