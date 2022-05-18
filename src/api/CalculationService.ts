4e22b4fa0378ab9dc9efb9800dbdba12a80b58ed
Nate Moraca <nate.moraca@semanticbits.com>
2022-05-18 16:09:24 -0400

    remove commented code

--- src/api/CalculationService.ts
+++ src/api/CalculationService.ts
@@ -5,9 +5,6 @@ import {
 } from "fqm-execution/build/types/Calculator";
 import TestCase from "../models/TestCase";
 import Measure from "../models/Measure";
-import { FHIRHelpers } from "../util/FHIRHelpers";
-import { getFhirMeasurePopulationCode } from "../util/PopulationsMap";
-import { PopulationType } from "../models/MeasurePopulation";
 
 // TODO consider converting into a context.
 // OR a re-usable hook.
@@ -17,7 +14,6 @@ export class CalculationService {
     testCases: TestCase[],
     measureBundle: fhir4.Bundle
   ): Promise<ExecutionResult[]> {
-    // const measureBundle2 = this.buildMeasureBundle(measure);
     const TestCaseBundles = testCases.map((testCase) => {
       return this.buildPatientBundle(testCase);
     });
@@ -30,99 +26,6 @@ export class CalculationService {
     );
     return calculationOutput?.results;
   }
-
-  // private buildMeasureBundle(measure: Measure): fhir4.Bundle {
-  //   const bundle: fhir4.Bundle = {
-  //     resourceType: "Bundle",
-  //     type: "transaction",
-  //     entry: [
-  //       // Measure Resource
-  //       {
-  //         resource: {
-  //           resourceType: "Measure",
-  //           status: "draft", //TODO convert measure.state to status enum.
-  //           library: [
-  //             `http://ecqi.healthit.gov/ecqms/Library/${measure.cqlLibraryName}`,
-  //           ],
-  //           group: [],
-  //         },
-  //         request: { method: "PUT", url: `Measure/${measure.cqlLibraryName}` },
-  //       },
-  //       // Measure Library Resource
-  //       {
-  //         resource: {
-  //           resourceType: "Library",
-  //           url: `http://ecqi.healthit.gov/ecqms/Library/${measure.cqlLibraryName}`,
-  //           status: "active",
-  //           type: {
-  //             coding: [
-  //               {
-  //                 system: "http://terminology.hl7.org/CodeSystem/library-type",
-  //                 code: "logic-library",
-  //               },
-  //             ],
-  //           },
-  //           content: [
-  //             { contentType: "text/cql", data: `${btoa(measure.cql)}` },
-  //             {
-  //               contentType: "application/elm+json",
-  //               data: `${btoa(measure.elmJson)}`,
-  //             },
-  //           ],
-  //         },
-  //         request: { method: "PUT", url: `Library/${measure.cqlLibraryName}` },
-  //       },
-  //       // FHIR Helpers
-  //       { ...FHIRHelpers },
-  //     ],
-  //   };
-  //   const measureResource: fhir4.Measure = bundle.entry.find(
-  //     (e) => e.resource.resourceType === "Measure"
-  //   ).resource as fhir4.Measure;
-  //
-  //   this.buildMeasureGroups(measure, measureResource);
-  //
-  //   return bundle;
-  // }
-  //
-  // private buildMeasureGroups(measure: Measure, measureResource: fhir4.Measure) {
-  //   // verify if measureGroup is created? if not Calculation will fail, so return to user specifying to add atleast one measureGroup
-  //   measure.groups.map((group) => {
-  //     const fhirMeasureGroup: fhir4.MeasureGroup = {};
-  //     fhirMeasureGroup.population = this.buildMeasureGroup(group.population);
-  //     measureResource.group.push(fhirMeasureGroup);
-  //   });
-  // }
-  //
-  // private buildMeasureGroup(
-  //   measurePopulations: PopulationType
-  // ): fhir4.MeasureGroupPopulation[] {
-  //   const msrPops: [string, string][] = Object.entries(measurePopulations);
-  //   return msrPops.map((population) => {
-  //     return this.buildMeasureGroupPopulation(population);
-  //   });
-  // }
-  //
-  // private buildMeasureGroupPopulation(
-  //   measurePopulation: [string, string]
-  // ): fhir4.MeasureGroupPopulation {
-  //   return {
-  //     code: {
-  //       coding: [
-  //         {
-  //           system: "http://terminology.hl7.org/CodeSystem/measure-population",
-  //           code: getFhirMeasurePopulationCode(measurePopulation[0]),
-  //           display: measurePopulation[0],
-  //         },
-  //       ],
-  //     },
-  //     criteria: {
-  //       language: "text/cql.identifier",
-  //       // TODO Use correct display content.
-  //       expression: measurePopulation[1],
-  //     },
-  //   };
-  // }
 
   // fqm Execution requires each patient to be with unique ID.
   // So assigning the testCase ID as patient ID to retrieve calculate multiple testcases
