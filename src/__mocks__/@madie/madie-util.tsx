5da2e240af00a5597ddfd885c8bbd437948022f3
adongare <ashok.dongare@semanticbits.com>
2022-06-21 10:51:34 -0400

    updated mock(not being used but needed)

--- src/__mocks__/@madie/madie-util.tsx
+++ src/__mocks__/@madie/madie-util.tsx
@@ -1,12 +1,6 @@
-// export const getServiceConfig = () => ({
-//   measureService: {
-//     baseUrl: "example-service-url",
-//   },
-// });
-//
-// export const measureStore = {
-//   initialState: null,
-//   state: null,
-//   subscribe: () => null, // needs to return an object with key subscribe
-//   unsubscribe: () => null,
-// };
+export const measureStore = {
+  initialState: null,
+  state: null,
+  subscribe: () => null,
+  unsubscribe: () => null,
+};
