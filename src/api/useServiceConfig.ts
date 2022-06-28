d59aea0846239c77a6de635330eb029eea96bafb
sb-prateekkeerthi <prateek.keerthi@semanticbits.com>
2022-06-28 11:19:38 -0400

    testing

--- src/api/useServiceConfig.ts
+++ src/api/useServiceConfig.ts
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import ServiceContext, { ServiceConfig } from "./ServiceContext";
 
+
 export default function useServiceConfig(): ServiceConfig {
   return useContext(ServiceContext);
 }
