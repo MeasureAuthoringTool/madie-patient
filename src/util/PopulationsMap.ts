b3bd4c1d140a1e6181a6e659d4e86ba5efda3fd1
Ethan Kaplan <phaladone@gmail.com>
2022-09-30 13:03:25 -0700

    Removed dead file, fixed numbering bug

--- src/util/PopulationsMap.ts
+++ src/util/PopulationsMap.ts
@@ -85,244 +85,256 @@ export function triggerPopChanges(
       popMap[value.name] = value;
     }
   );
-if(targetPopulation.populationBasis === "Boolean"){
-  if (
-    targetPopulation.scoring === "Continuous Variable" ||
-    targetPopulation.scoring === "Ratio"
-  ) {
-    //removing observations
+  if (targetPopulation.populationBasis === "Boolean") {
     if (
-      changedPopulationName === "measurePopulationExclusion" &&
-      expectedValue === true
+      targetPopulation.scoring === "Continuous Variable" ||
+      targetPopulation.scoring === "Ratio"
     ) {
-      targetPopulation.populationValues =
-        targetPopulation.populationValues.filter(
-          (population) => population.name !== "measureObservation"
-        );
-    }
-
-    if (
-      (changedPopulationName === "numeratorExclusion" ||
-        changedPopulationName === "denominatorExclusion") &&
-      expectedValue === true
-    ) {
-      const linkedPopulationName =
-        changedPopulationName === "numeratorExclusion"
-          ? "numerator"
-          : "denominator";
-      const linkedPopulationId = targetPopulation.populationValues.filter(
-        (target) => target.name === linkedPopulationName
-      )[0].id;
-      if (linkedPopulationId) {
+      //removing observations
+      if (
+        changedPopulationName === "measurePopulationExclusion" &&
+        expectedValue === true
+      ) {
         targetPopulation.populationValues =
           targetPopulation.populationValues.filter(
-            (population) => population.criteriaReference !== linkedPopulationId
+            (population) => population.name !== "measureObservation"
           );
       }
-    }
 
-    //adding the observation(after removal)
-    if (
-      changedPopulationName === "measurePopulationExclusion" &&
-      expectedValue === false
-    ) {
-      const measureObservationId = measureGroups.filter(
-        (group) => group.id === changedGroupId
-      )[0].measureObservations[0].id;
-      targetPopulation.populationValues.push({
-        name: PopulationType.MEASURE_OBSERVATION,
-        expected: false,
-        id: measureObservationId,
-        criteriaReference: undefined,
-      });
-    }
-
-    if (
-      (changedPopulationName === "numeratorExclusion" ||
-        changedPopulationName === "denominatorExclusion") &&
-      expectedValue === false
-    ) {
-      const linkedPopulationName =
-        changedPopulationName === "numeratorExclusion"
-          ? "numerator"
-          : "denominator";
-      const criteriaReferenceID = targetPopulation.populationValues.filter(
-        (population) => population.name === linkedPopulationName
-      )[0].id;
-
-      const changedPopulationObservations = measureGroups.filter(
-        (group) => group.id === changedGroupId
-      )[0].measureObservations;
-
-      if (changedPopulationObservations && criteriaReferenceID) {
-        const measureObservationId = changedPopulationObservations.filter(
-          (observation) => observation.criteriaReference === criteriaReferenceID
+      if (
+        (changedPopulationName === "numeratorExclusion" ||
+          changedPopulationName === "denominatorExclusion") &&
+        expectedValue === true
+      ) {
+        const linkedPopulationName =
+          changedPopulationName === "numeratorExclusion"
+            ? "numerator"
+            : "denominator";
+        const linkedPopulationId = targetPopulation.populationValues.filter(
+          (target) => target.name === linkedPopulationName
         )[0].id;
-
-        const numeratorMeasureObservationIndex =
-          targetPopulation.populationValues.findIndex((prop) => {
-            return prop.name === "measureObservation";
-          });
-
-        //always adding denominator obseravtion before numerator observation
-        if (
-          changedPopulationName === "denominatorExclusion" &&
-          numeratorMeasureObservationIndex > -1
-        ) {
-          const denominatorMeasureObservation = {
-            name: PopulationType.MEASURE_OBSERVATION,
-            expected: false,
-            id: measureObservationId,
-            criteriaReference: criteriaReferenceID,
-          };
-          targetPopulation.populationValues.splice(
-            numeratorMeasureObservationIndex,
-            0,
-            denominatorMeasureObservation
-          );
-        } else {
-          targetPopulation.populationValues.push({
-            name: PopulationType.MEASURE_OBSERVATION,
-            expected: false,
-            id: measureObservationId,
-            criteriaReference: criteriaReferenceID,
-          });
+        if (linkedPopulationId) {
+          targetPopulation.populationValues =
+            targetPopulation.populationValues.filter(
+              (population) =>
+                population.criteriaReference !== linkedPopulationId
+            );
         }
       }
-    }
-  }
-}else{if (
-  targetPopulation.scoring === "Continuous Variable" ||
-  targetPopulation.scoring === "Ratio"
-) {
-  if (
-    changedPopulationName === "measurePopulationExclusion" ||
-    changedPopulationName === "measurePopulation"
-  ) {
-    const measurePopulation = Number(
-        targetPopulation.populationValues[1].expected
-      ),
-      measurePopulationEx =targetPopulation.populationValues[2].name==="measurePopulationExclusion"?Number(
-        targetPopulation.populationValues[2].expected):0
-      ;
-        const measPopLen = targetPopulation.populationValues[2].name==="measurePopulationExclusion"?4:3
-    if (
-      targetPopulation.populationValues.length <
-      measPopLen + measurePopulation - measurePopulationEx
-    ) {
-      const measureObservationId = measureGroups.filter(
-        (group) => group.id === changedGroupId
-      )[0].measureObservations[0].id;
-      while (
-        targetPopulation.populationValues.length <
-        measPopLen + measurePopulation - measurePopulationEx
+
+      //adding the observation(after removal)
+      if (
+        changedPopulationName === "measurePopulationExclusion" &&
+        expectedValue === false
       ) {
+        const measureObservationId = measureGroups.filter(
+          (group) => group.id === changedGroupId
+        )[0].measureObservations[0].id;
         targetPopulation.populationValues.push({
           name: PopulationType.MEASURE_OBSERVATION,
-          expected: 0,
-          id:
-            `Observation` +
-            (targetPopulation.populationValues.length -
-              (measurePopulation - measurePopulationEx)),
+          expected: false,
+          id: measureObservationId,
           criteriaReference: undefined,
         });
       }
-    } else if (
-      targetPopulation.populationValues.length >
-      measPopLen + measurePopulation - measurePopulationEx
-    ) {
-      if (measurePopulation - measurePopulationEx >= 0) {
-        while (
-          targetPopulation.populationValues.length >
-          measPopLen + measurePopulation - measurePopulationEx
-        ) {
-          targetPopulation.populationValues.pop();
+
+      if (
+        (changedPopulationName === "numeratorExclusion" ||
+          changedPopulationName === "denominatorExclusion") &&
+        expectedValue === false
+      ) {
+        const linkedPopulationName =
+          changedPopulationName === "numeratorExclusion"
+            ? "numerator"
+            : "denominator";
+        const criteriaReferenceID = targetPopulation.populationValues.filter(
+          (population) => population.name === linkedPopulationName
+        )[0].id;
+
+        const changedPopulationObservations = measureGroups.filter(
+          (group) => group.id === changedGroupId
+        )[0].measureObservations;
+
+        if (changedPopulationObservations && criteriaReferenceID) {
+          const measureObservationId = changedPopulationObservations.filter(
+            (observation) =>
+              observation.criteriaReference === criteriaReferenceID
+          )[0].id;
+
+          const numeratorMeasureObservationIndex =
+            targetPopulation.populationValues.findIndex((prop) => {
+              return prop.name === "measureObservation";
+            });
+
+          //always adding denominator obseravtion before numerator observation
+          if (
+            changedPopulationName === "denominatorExclusion" &&
+            numeratorMeasureObservationIndex > -1
+          ) {
+            const denominatorMeasureObservation = {
+              name: PopulationType.MEASURE_OBSERVATION,
+              expected: false,
+              id: measureObservationId,
+              criteriaReference: criteriaReferenceID,
+            };
+            targetPopulation.populationValues.splice(
+              numeratorMeasureObservationIndex,
+              0,
+              denominatorMeasureObservation
+            );
+          } else {
+            targetPopulation.populationValues.push({
+              name: PopulationType.MEASURE_OBSERVATION,
+              expected: false,
+              id: measureObservationId,
+              criteriaReference: criteriaReferenceID,
+            });
+          }
         }
       }
     }
-  } else if (
-    changedPopulationName === "numeratorExclusion" ||
-    changedPopulationName === "numerator" ||
-    changedPopulationName === "denominatorExclusion" ||
-    changedPopulationName === "denominator"
-  ) {
-    const numExIn = targetPopulation.populationValues.findIndex((prop) => {
-      return prop.name === "numeratorExclusion";
-    });
-    const numIn = targetPopulation.populationValues.findIndex((prop) => {
-      return prop.name === "numerator";
-    });
-    const denomExIn = targetPopulation.populationValues.findIndex(
-      (prop) => {
-        return prop.name === "denominatorExclusion";
-      }
-    );
-    const denomIn = targetPopulation.populationValues.findIndex((prop) => {
-      return prop.name === "denominator";
-    });
-    const num = Number(targetPopulation.populationValues[numIn].expected),
-      numEx = targetPopulation.populationValues[numExIn]
-        ? Number(targetPopulation.populationValues[numExIn].expected)
-        : 0,
-      denom = Number(targetPopulation.populationValues[denomIn].expected),
-      denomEx = targetPopulation.populationValues[denomExIn]
-        ? Number(targetPopulation.populationValues[denomExIn].expected)
-        : 0;
-
-    const headLength =
-      3 + (numExIn > -1 ? 1 : 0) + (denomExIn > -1 ? 1 : 0);
-    const numLen = num >= numEx ? num - numEx : 0;
-    const denomLen = denom >= denomEx ? denom - denomEx : 0;
-
-    const newLen = headLength + denomLen + numLen;
-
+  } else {
     if (
-      (changedPopulationName === "numerator" && numExIn > -1) ||
-      changedPopulationName === "numeratorExclusion"
+      targetPopulation.scoring === "Continuous Variable" ||
+      targetPopulation.scoring === "Ratio"
     ) {
-      if (newLen > targetPopulation.populationValues.length) {
-        //ratio insert
-        while (targetPopulation.populationValues.length < newLen) {
-          targetPopulation.populationValues.push({
-            name: PopulationType.MEASURE_OBSERVATION,
-            expected: 0,
-            id:
-              "numeratorObservation" +
-              (targetPopulation.populationValues.length - denomLen),
-            criteriaReference: null,
-          });
+      if (
+        changedPopulationName === "measurePopulationExclusion" ||
+        changedPopulationName === "measurePopulation"
+      ) {
+        const measurePopulation = Number(
+            targetPopulation.populationValues[1].expected
+          ),
+          measurePopulationEx =
+            targetPopulation.populationValues[2].name ===
+            "measurePopulationExclusion"
+              ? Number(targetPopulation.populationValues[2].expected)
+              : 0;
+        const measurePopDif =
+          measurePopulationEx < measurePopulation
+            ? measurePopulation - measurePopulationEx
+            : 0;
+        const measPopLen =
+          targetPopulation.populationValues[2].name ===
+          "measurePopulationExclusion"
+            ? 4
+            : 3;
+
+        if (
+          targetPopulation.populationValues.length <
+          measPopLen + measurePopDif
+        ) {
+          const measureObservationId = measureGroups.filter(
+            (group) => group.id === changedGroupId
+          )[0].measureObservations[0].id;
+          while (
+            targetPopulation.populationValues.length <
+            measPopLen + measurePopDif
+          ) {
+            targetPopulation.populationValues.push({
+              name: PopulationType.MEASURE_OBSERVATION,
+              expected: 0,
+              id:
+                `Observation` +
+                (targetPopulation.populationValues.length -
+                  (measurePopulation - measurePopulationEx)),
+              criteriaReference: undefined,
+            });
+          }
+        } else if (
+          targetPopulation.populationValues.length >
+          measPopLen + measurePopDif
+        ) {
+          while (
+            targetPopulation.populationValues.length >
+            measPopLen + measurePopDif
+          ) {
+            targetPopulation.populationValues.pop();
+          }
         }
-      } else if (newLen < targetPopulation.populationValues.length) {
-        while (targetPopulation.populationValues.length > newLen) {
-          targetPopulation.populationValues.pop();
-        }
-      }
-    } else {
-      if (newLen > targetPopulation.populationValues.length) {
-        let insertPoint = targetPopulation.populationValues.length - numLen;
-        while (targetPopulation.populationValues.length < newLen) {
-          targetPopulation.populationValues.splice(insertPoint, 0, {
-            name: PopulationType.MEASURE_OBSERVATION,
-            expected: 0,
-            id:
-              "denominatorObservation" +
-              (targetPopulation.populationValues.length - numLen),
-            criteriaReference: null,
-          });
-          insertPoint++;
-        }
-      } else if (newLen < targetPopulation.populationValues.length) {
-        let delPoint =
-          targetPopulation.populationValues.length - numLen - 1;
-        while (targetPopulation.populationValues.length > newLen) {
-          targetPopulation.populationValues.splice(delPoint, 1);
-          delPoint--;
+      } else if (
+        changedPopulationName === "numeratorExclusion" ||
+        changedPopulationName === "numerator" ||
+        changedPopulationName === "denominatorExclusion" ||
+        changedPopulationName === "denominator"
+      ) {
+        const numExIn = targetPopulation.populationValues.findIndex((prop) => {
+          return prop.name === "numeratorExclusion";
+        });
+        const numIn = targetPopulation.populationValues.findIndex((prop) => {
+          return prop.name === "numerator";
+        });
+        const denomExIn = targetPopulation.populationValues.findIndex(
+          (prop) => {
+            return prop.name === "denominatorExclusion";
+          }
+        );
+        const denomIn = targetPopulation.populationValues.findIndex((prop) => {
+          return prop.name === "denominator";
+        });
+        const num = Number(targetPopulation.populationValues[numIn].expected),
+          numEx = targetPopulation.populationValues[numExIn]
+            ? Number(targetPopulation.populationValues[numExIn].expected)
+            : 0,
+          denom = Number(targetPopulation.populationValues[denomIn].expected),
+          denomEx = targetPopulation.populationValues[denomExIn]
+            ? Number(targetPopulation.populationValues[denomExIn].expected)
+            : 0;
+
+        const headLength =
+          3 + (numExIn > -1 ? 1 : 0) + (denomExIn > -1 ? 1 : 0);
+        const numLen = num >= numEx ? num - numEx : 0;
+        const denomLen = denom >= denomEx ? denom - denomEx : 0;
+
+        const newLen = headLength + denomLen + numLen;
+
+        if (
+          (changedPopulationName === "numerator" && numExIn > -1) ||
+          changedPopulationName === "numeratorExclusion"
+        ) {
+          if (newLen > targetPopulation.populationValues.length) {
+            //ratio insert
+            while (targetPopulation.populationValues.length < newLen) {
+              targetPopulation.populationValues.push({
+                name: PopulationType.MEASURE_OBSERVATION,
+                expected: 0,
+                id:
+                  "numeratorObservation" +
+                  (targetPopulation.populationValues.length - denomLen),
+                criteriaReference: null,
+              });
+            }
+          } else if (newLen < targetPopulation.populationValues.length) {
+            while (targetPopulation.populationValues.length > newLen) {
+              targetPopulation.populationValues.pop();
+            }
+          }
+        } else {
+          if (newLen > targetPopulation.populationValues.length) {
+            let insertPoint = targetPopulation.populationValues.length - numLen;
+            while (targetPopulation.populationValues.length < newLen) {
+              targetPopulation.populationValues.splice(insertPoint, 0, {
+                name: PopulationType.MEASURE_OBSERVATION,
+                expected: 0,
+                id:
+                  "denominatorObservation" +
+                  (targetPopulation.populationValues.length - numLen),
+                criteriaReference: null,
+              });
+              insertPoint++;
+            }
+          } else if (newLen < targetPopulation.populationValues.length) {
+            let delPoint =
+              targetPopulation.populationValues.length - numLen - 1;
+            while (targetPopulation.populationValues.length > newLen) {
+              targetPopulation.populationValues.splice(delPoint, 1);
+              delPoint--;
+            }
+          }
         }
       }
     }
   }
-}
-}
 
   if (targetPopulation.scoring === "Proportion") {
     //denominator
