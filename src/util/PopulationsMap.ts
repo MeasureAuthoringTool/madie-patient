b1c934ffef1608fb651c7664381b8f19b2e7784b
Ethan Kaplan <phaladone@gmail.com>
2022-10-03 10:39:49 -0700

    Changed CV based on Stan feedback

--- src/util/PopulationsMap.ts
+++ src/util/PopulationsMap.ts
@@ -213,11 +213,7 @@ export function triggerPopChanges(
           measurePopulationEx < measurePopulation
             ? measurePopulation - measurePopulationEx
             : 0;
-        const measPopLen =
-          targetPopulation.populationValues[2].name ===
-          "measurePopulationExclusion"
-            ? 4
-            : 3;
+        const measPopLen =3;
 
         if (
           targetPopulation.populationValues.length <
