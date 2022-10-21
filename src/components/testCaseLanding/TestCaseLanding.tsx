e9d1fafa607fc8e16185bf2ea09e0956cacc3f0d
Rohit Kandimalla <rohit.kandimalla@semanticbits.com>
2022-10-21 16:07:43 -0400

    updated title for test case list page

--- src/components/testCaseLanding/TestCaseLanding.tsx
+++ src/components/testCaseLanding/TestCaseLanding.tsx
@@ -5,7 +5,7 @@ import TestCaseList from "../testCaseList/TestCaseList";
 import { useDocumentTitle } from "@madie/madie-util";
 
 const TestCaseLanding = () => {
-  useDocumentTitle("MADiE Edit Measure Test Case");
+  useDocumentTitle("MADiE Edit Measure Test Cases");
   return (
     <div tw="m-2">
       <section>
