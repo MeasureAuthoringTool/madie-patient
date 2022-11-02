8182e8caa064f930a056c4356f4cf6d6a59c2d58
Joseph Kotanchik <joseph.kotanchik@semanticbits.com>
2022-11-02 15:08:44 -0400

    Remove the extra run button click in unit test

--- src/components/editTestCase/EditTestCase.test.tsx
+++ src/components/editTestCase/EditTestCase.test.tsx
@@ -2263,11 +2263,12 @@ describe("Measure Calculation ", () => {
     )) as HTMLInputElement;
     userEvent.clear(editor);
     await waitFor(() => expect(editor.value).toBe(""));
-    userEvent.paste(editor, `   `);
+    // paste is either including the original editor.value content or
+    //  somehow the cleared editor state is reverted to its original.
+    userEvent.paste(editor, "    ");
     await waitFor(() => expect(editor.value).toBeTruthy());
 
     const runButton = await screen.findByRole("button", { name: "Run Test" });
-    userEvent.click(runButton);
     await waitFor(async () => userEvent.click(runButton));
 
     userEvent.click(screen.getByTestId("highlighting-tab"));
