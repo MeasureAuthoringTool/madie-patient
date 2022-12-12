e38913d54e829ecd6197f523640e0750c9c5c3e6
adongare <ashok.dongare@semanticbits.com>
2022-12-12 13:10:34 -0500

    removed unused import

--- src/components/testCaseList/CodeCoverageHighlighting.tsx
+++ src/components/testCaseList/CodeCoverageHighlighting.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import "twin.macro";
 import "styled-components/macro";
 import parse from "html-react-parser";
-import { Alert } from "@madie/madie-design-system/dist/react";
 
 function CodeCoverageHighlighting({ coverageHTML }) {
   return (
