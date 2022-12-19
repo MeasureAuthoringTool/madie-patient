3641a419dfa66538b7c3e32dabe6fe199991b5f4
CeciliaLiu8 <cecilialiu8@gmail.com>
2022-12-19 12:05:26 -0600

    simply status handler

--- src/components/statusHandler/StatusHandler.tsx
+++ src/components/statusHandler/StatusHandler.tsx
@@ -4,44 +4,40 @@ import "./StatusHandler.scss";
 
 const StatusHandler = ({ error, errorMessages, testDataId }) => {
   {
-    if (error) {
-      if (errorMessages) {
-        if (errorMessages.length === 1) {
-          return (
-            <MadieAlert
-              data-testid="generic-error-text-header"
-              type="error"
-              content={
-                <h3 aria-live="polite" role="alert" data-testid={testDataId}>
-                  {errorMessages}
-                </h3>
-              }
-              canClose={false}
-            />
-          );
-        } else if (errorMessages.length > 1) {
-          const mappedMessages = errorMessages.map(
-            (em: string, index: number) => <ol key={index}>{em}</ol>
-          );
-          return (
-            <MadieAlert
-              type="error"
-              content={
-                <div aria-live="polite" role="alert" data-testid={testDataId}>
-                  <h3>{errorMessages.length} errors were found</h3>
-                  <ul data-testid="generic-fail-text-list">{mappedMessages}</ul>
-                </div>
-              }
-              canClose={false}
-            />
-          );
-        } else {
-          return <div />;
-        }
+    if (error && errorMessages) {
+      //if (errorMessages) {
+      if (errorMessages.length === 1) {
+        return (
+          <MadieAlert
+            data-testid="generic-error-text-header"
+            type="error"
+            content={
+              <h3 aria-live="polite" role="alert" data-testid={testDataId}>
+                {errorMessages}
+              </h3>
+            }
+            canClose={false}
+          />
+        );
+      } else if (errorMessages.length > 1) {
+        const mappedMessages = errorMessages.map(
+          (em: string, index: number) => <ol key={index}>{em}</ol>
+        );
+        return (
+          <MadieAlert
+            type="error"
+            content={
+              <div aria-live="polite" role="alert" data-testid={testDataId}>
+                <h3>{errorMessages.length} errors were found</h3>
+                <ul data-testid="generic-fail-text-list">{mappedMessages}</ul>
+              </div>
+            }
+            canClose={false}
+          />
+        );
       }
-    } else {
-      return <div />;
     }
+    return <div />;
   }
 };
 
