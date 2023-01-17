6f3b69fc2e898b3958eacfd8da2f86dd3acbf9a8
Ethan Kaplan <phaladone@gmail.com>
2023-01-17 12:15:14 -0800

    Add api/utils for featureflag

---
+++ src/api/utils/useFeatureFlag.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { ServiceConfig } from "../api/ServiceContext";
+import useServiceConfig from "../api/useServiceConfig";
+
+/**
+ * Get the current state of the target feature flag.
+ * @See feature flags in Service Config JSON.
+ * @param feature: string name of the target flag.
+ * @returns enabled: boolean true, the feature has been enabled.
+ *    False, the target feature is disabled or the flag was not found.
+ */
+export default function useFeature(feature: string): boolean {
+  const serviceConfig: ServiceConfig = useServiceConfig();
+  return serviceConfig?.features?.[feature] ?? false;
+}
