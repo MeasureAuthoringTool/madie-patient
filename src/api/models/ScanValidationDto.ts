504f73d331b907bd3c477b284d644ae996a26874
Nate Moraca <nate.moraca@semanticbits.com>
2023-01-27 11:20:25 -0500

    add virus specific scan response dto

---
+++ src/api/models/ScanValidationDto.ts
@@ -0,0 +1,12 @@
+export interface ScanValidationDto {
+  fileName: string;
+  valid: boolean;
+  error: ObjectError;
+}
+
+export interface ObjectError {
+  codes: string[],
+  defaultMessage: string,
+  objectName: string;
+  code: string;
+}
