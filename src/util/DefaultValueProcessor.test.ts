ad1304d80b27bdc3cf93cba76c611f2a00c6f284
Rohit Kandimalla <rohit.kandimalla@semanticbits.com>
2023-02-03 13:34:54 -0500

    Added defaults for Pratitioner resource

--- src/util/DefaultValueProcessor.test.ts
+++ src/util/DefaultValueProcessor.test.ts
@@ -1,5 +1,6 @@
 import _ from "lodash";
 import { addValues } from "./DefaultValueProcessor";
+import { Practitioner } from "fhir/r4";
 
 describe("Modify JSON to add Default Values", () => {
   it('should set Coverage.status to "active" in the TestCase', () => {
@@ -115,5 +116,55 @@ describe("Modify JSON to add Default Values", () => {
     expect(results).toBeDefined();
 
     expect(results.length).toBe(1);
+  });
+
+  it("Should set identifier and name attributes to Practitioner", () => {
+    const defaultFamilyName = "Evil";
+    const defaultPrefix = "Dr";
+    const defaultIdentifierSystem = "http://hl7.org/fhir/sid/us-npi";
+    const defaultIdentifierValue = "123456";
+
+    const testCaseWithPractitioner = require("../mockdata/testCase_with_Practitioner.json");
+    const updatedTestCaseWithDefaults: any = addValues(
+      testCaseWithPractitioner
+    );
+
+    expect(updatedTestCaseWithDefaults).toBeDefined();
+    const practitioners: Practitioner[] = [];
+    _.forEach(
+      updatedTestCaseWithDefaults?.entry,
+      (entry) =>
+        entry.resource?.resourceType === "Practitioner" &&
+        practitioners.push(entry.resource)
+    );
+    expect(practitioners.length).toBe(4);
+
+    // Nothing should be changed for 1st practitioner
+    expect(practitioners[0].name[0].family).toBe("Careful");
+    expect(practitioners[0].name[0].prefix[0]).toBe("Dr");
+    expect(practitioners[0].identifier[0].system).toBe(
+      "urn:oid:2.16.840.1.113883.4.336"
+    );
+    expect(practitioners[0].identifier[0].value).toBe("Practitioner-23");
+
+    // name and identifier defaults are added to 2nd practitioner
+    expect(practitioners[1].name[0].family).toBe(defaultFamilyName);
+    expect(practitioners[1].name[0].prefix[0]).toBe(defaultPrefix);
+    expect(practitioners[1].identifier[0].system).toBe(defaultIdentifierSystem);
+    expect(practitioners[1].identifier[0].value).toBe(defaultIdentifierValue);
+
+    // only default name is added to 3rd practitioner
+    expect(practitioners[2].name[0].family).toBe(defaultFamilyName);
+    expect(practitioners[2].name[0].prefix[0]).toBe(defaultPrefix);
+    expect(practitioners[2].identifier[0].system).toBe(
+      "urn:oid:2.16.840.1.113883.4.336"
+    );
+    expect(practitioners[2].identifier[0].value).toBe("Practitioner-23");
+
+    // only default identifier is added to 4th practitioner
+    expect(practitioners[3].name[0].family).toBe("Careful");
+    expect(practitioners[3].name[0].prefix[0]).toBe("Dr");
+    expect(practitioners[3].identifier[0].system).toBe(defaultIdentifierSystem);
+    expect(practitioners[3].identifier[0].value).toBe(defaultIdentifierValue);
   });
 });
