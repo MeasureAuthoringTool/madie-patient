fd624ad702d793c7171d61d928a1fdc1d67f5b6f
Nate Moraca <nate.moraca@semanticbits.com>
2023-02-10 08:59:00 -0500

    remove debug statement from jsx

--- src/components/testCaseList/TestCaseList.tsx
+++ src/components/testCaseList/TestCaseList.tsx
@@ -295,7 +295,6 @@ const TestCaseList = (props: TestCaseListProps) => {
       tw="grid lg:grid-cols-6 gap-4 mx-8 my-6 shadow-lg rounded-md border border-slate bg-white"
       style={{ marginTop: 16 }}
     >
-      <span>canEdit: {canEdit ? "yes" : "no"}</span>
       {!loadingState.loading && (
         <>
           <TestCaseListSideBarNav
