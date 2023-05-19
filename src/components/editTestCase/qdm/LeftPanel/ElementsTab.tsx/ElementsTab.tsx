9445e45144e74730e4995e1c376ac14f50f18064
mcmcphillips <matt.mcphillips@semanticbits.com>
2023-05-18 17:07:46 -0700

    Add elements back in

--- src/components/editTestCase/qdm/LeftPanel/ElementsTab.tsx/ElementsTab.tsx
+++ src/components/editTestCase/qdm/LeftPanel/ElementsTab.tsx/ElementsTab.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import DemographicsSection from "./Demographics/DemographicsSection";
-
+import ElementsSection from "./Elements/ElementsSection";
 const ElementsTab = ({ canEdit }) => {
   return (
     <>
       <DemographicsSection canEdit={canEdit} />
-      {/* < */}
+      <ElementsSection />
     </>
   );
 };
