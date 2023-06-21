bafcd2c2574079052a6b6628e1d422129dc01a34
Keerthi <43103@icfxy4km6vvxy.lan>
2023-06-21 12:49:10 -0400

    removed unwanted code

--- tailwind.config.js
+++ tailwind.config.js
@@ -133,9 +133,6 @@ module.exports = {
         96: "24rem",
         128: "32rem",
       },
-      width: {
-        64: "19rem",
-      },
       fontFamily: {
         rubik: ["Rubik"],
       },
