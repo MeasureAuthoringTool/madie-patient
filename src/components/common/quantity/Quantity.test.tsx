7ce1ec1ac542553a1d9a148c3923a584f86aba7e
Christopher Hubert <christopher.hubert@semanticbits.com>
2023-06-28 01:07:37 -0400

    add quantity component

---
+++ src/components/common/quantity/Quantity.test.tsx
@@ -0,0 +1,123 @@
+import React, { useState } from "react";
+import {
+  render,
+  screen,
+  within,
+  fireEvent,
+  waitFor,
+} from "@testing-library/react";
+import { act } from "react-dom/test-utils";
+import Quantity from "./Quantity";
+
+describe("Quantity Component", () => {
+  const options = [
+    {
+      code: "B[V]",
+      guidance: "used to express power gain in electrical circuits",
+      name: "bel volt",
+      system: "https://clinicaltables.nlm.nih.gov/",
+    },
+    {
+      code: "B",
+      guidance:
+        "Logarithm of the ratio of power- or field-type quantities; usually expressed in decibels ",
+      name: "bel",
+      system: "https://clinicaltables.nlm.nih.gov/",
+    },
+    {
+      code: "mho",
+      guidance:
+        "unit of electric conductance (the inverse of electrical resistance) equal to ohm^-1",
+      name: "mho",
+      system: "https://clinicaltables.nlm.nih.gov/",
+    },
+  ];
+  const testValue = {
+    value: {
+      code: "mho",
+      guidance:
+        "unit of electric conductance (the inverse of electrical resistance) equal to ohm^-1",
+      name: "mho",
+      system: "https://clinicaltables.nlm.nih.gov/",
+    },
+  };
+
+  test("Should render quantity component", () => {
+    const handleChange = jest.fn();
+    render(
+      <Quantity
+        quantityUnit={"Test Option1"}
+        handleQuantityUnitChange={handleChange}
+        canEdit={true}
+        placeholder="test"
+        quantityValue={0}
+        handleQuantityValueChange={handleChange}
+        options={options}
+      />
+    );
+    const quantityUnitLabel = screen.getByTestId(
+      "quantity-unit-dropdown-label"
+    );
+    expect(quantityUnitLabel).toBeInTheDocument();
+    const quantityUnitInput = screen.getByRole("combobox");
+    expect(quantityUnitInput).toBeInTheDocument();
+
+    const quantityValueLabel = screen.getByTestId("quantity-value-label");
+    expect(quantityValueLabel).toBeInTheDocument();
+    const quantityValueInput = screen.getByRole("textbox");
+    expect(quantityValueInput).toBeInTheDocument();
+  });
+
+  test("Should render quantity unit field with selected option", async () => {
+    const handleChange = jest.fn();
+    render(
+      <Quantity
+        quantityUnit={testValue}
+        handleQuantityUnitChange={handleChange}
+        canEdit={true}
+        placeholder="test"
+        quantityValue={0}
+        handleQuantityValueChange={handleChange}
+        options={options}
+      />
+    );
+    await act(async () => {
+      const quantityUnitLabel = screen.getByTestId(
+        "quantity-unit-dropdown-label"
+      );
+      expect(quantityUnitLabel).toBeInTheDocument();
+      const quantityAutoComplete = await screen.findByTestId(
+        "quantity-unit-dropdown"
+      );
+      const listBox = within(quantityAutoComplete).getByRole("combobox");
+      expect(listBox).toHaveValue(
+        `${testValue.value.code} ${testValue.value.name}`
+      );
+    });
+  });
+
+  test("Should render ucum options on click", async () => {
+    const handleChange = jest.fn();
+    render(
+      <Quantity
+        quantityUnit={null}
+        handleQuantityUnitChange={handleChange}
+        canEdit={true}
+        placeholder="test"
+        quantityValue={0}
+        handleQuantityValueChange={handleChange}
+        options={options}
+      />
+    );
+    const autocomplete = screen.getByTestId("quantity-unit-dropdown");
+    const input = within(autocomplete).getByRole("combobox");
+    autocomplete.click();
+    autocomplete.focus();
+    fireEvent.change(input, { target: { value: "b" } });
+    await act(async () => {
+      await new Promise((resolve) => setTimeout(resolve, 0));
+    });
+    fireEvent.click(screen.getAllByRole("option")[1]);
+    expect(input.value).toEqual("B bel");
+  });
+});
---
+++ src/components/common/quantity/Quantity.tsx
@@ -0,0 +1,202 @@
+import React, { useCallback, useEffect, useState } from "react";
+import { InputLabel } from "@madie/madie-design-system/dist/react/";
+import { Autocomplete, FormControl, TextField } from "@mui/material";
+import * as ucum from "@lhncbc/ucum-lhc";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+
+export const textFieldStyle = {
+  borderRadius: "3px",
+  height: 40,
+  width: 100,
+  border: "1px solid #DDDDDD",
+  "& .MuiOutlinedInput-notchedOutline": {
+    borderRadius: "3px",
+    "& legend": {
+      width: 0,
+    },
+  },
+  "& .MuiOutlinedInput-root": {
+    "&&": {
+      borderRadius: "3px",
+    },
+  },
+  "& .MuiInputBase-input": {
+    color: "#333",
+    fontFamily: "Rubik",
+    fontSize: 14,
+    borderRadius: "3px",
+    padding: "9px 5px 9px 5px",
+    "&::placeholder": {
+      opacity: 1,
+      color: "#717171",
+      fontFamily: "Rubik",
+      fontSize: 14,
+      padding: "9px 5px 9px 5px",
+    },
+  },
+};
+export interface QuantityProps {
+  quantityValue: number;
+  quantityUnit: any;
+  handleQuantityValueChange: Function;
+  handleQuantityUnitChange: Function;
+  options?: any;
+  canEdit: boolean;
+  placeholder: string;
+}
+
+const Quantity = ({
+  quantityValue,
+  quantityUnit,
+  handleQuantityValueChange,
+  handleQuantityUnitChange,
+  options,
+  canEdit,
+  placeholder,
+}: QuantityProps) => {
+  const autoCompleteStyles = {
+    borderRadius: "3px",
+    minHeight: "40px",
+    border: "1px solid #DDDDDD",
+    width: 100,
+    "& .MuiOutlinedInput-notchedOutline": {
+      borderRadius: "3px",
+      "& legend": {
+        width: 0,
+      },
+    },
+    "& .MuiAutocomplete-inputFocused": {
+      border: "none",
+      boxShadow: "none",
+      outline: "none",
+    },
+    "& .MuiAutocomplete-inputRoot": {
+      paddingTop: 0,
+      paddingBottom: 0,
+      backgroundColor: !canEdit ? "#EDEDED" : "",
+    },
+  };
+
+  const removeSpecificChars = (string) => {
+    return string.trim().replace("-", "").replace(".", "");
+  };
+
+  const parsedValue = quantityUnit?.value || null;
+
+  interface Option {
+    name: string;
+    code: string;
+    guidance: string;
+    system: string;
+  }
+
+  return (
+    <div>
+      <div>
+        <FormControl>
+          <InputLabel
+            id="quantity-value-label"
+            data-testid="quantity-value-label"
+            required={false}
+          >
+            Value
+          </InputLabel>
+          <TextField
+            value={quantityValue}
+            sx={textFieldStyle}
+            disabled={!canEdit}
+            label=""
+            id={"quantity-value-field"}
+            data-testid={"quantity-value-field"}
+            inputProps={{
+              "data-testid": "quantity-value-input",
+              "aria-describedby": "quantity-value-input-helper-text",
+              required: true,
+            }}
+            onChange={(newValue: any) => {
+              handleQuantityValueChange(newValue);
+            }}
+          />
+        </FormControl>
+        <FormControl>
+          <InputLabel
+            id="quantity-unit-dropdown-label"
+            data-testid="quantity-unit-dropdown-label"
+            required={false}
+          >
+            Unit
+          </InputLabel>
+          <Autocomplete
+            fullWidth={true}
+            disabled={!canEdit}
+            options={options}
+            data-testid="quantity-unit-dropdown"
+            popupIcon={<KeyboardArrowDownIcon sx={{ color: "#1C2556" }} />}
+            sx={autoCompleteStyles}
+            value={parsedValue}
+            getOptionLabel={(option: Option): string =>
+              `${option.code} ${option.name}`
+            }
+            disablePortal
+            limitTags={10}
+            filterOptions={(options, state): any[] => {
+              const { inputValue } = state;
+              if (inputValue) {
+                const input = removeSpecificChars(inputValue);
+                const filteredOptions = options.filter((opt: Option) => {
+                  const match = removeSpecificChars(`${opt.code} ${opt.name}`);
+                  return match.includes(input);
+                });
+                return filteredOptions;
+              }
+              return [];
+            }}
+            onChange={(event: any, values: any, reason: string) => {
+              if (values) {
+                const label = `${values.code} ${values.name}`;
+                const transformedResult = {
+                  label,
+                  value: values,
+                };
+                handleQuantityUnitChange(transformedResult);
+              } else {
+                handleQuantityUnitChange("");
+              }
+            }}
+            autoHighlight={true}
+            id="quantity-unit-dropdown"
+            renderInput={(params) => {
+              const { inputProps } = params;
+              inputProps["aria-labelledby"] = "scoring-unit-dropdown-label";
+              return (
+                <TextField
+                  {...params}
+                  data-testid="quantity-unit-text-input"
+                  inputProps={inputProps}
+                  disabled={!canEdit}
+                  sx={{
+                    input: {
+                      color: "#333",
+                      "&::placeholder": {
+                        opacity: 1,
+                        color: "#717171",
+                      },
+                    },
+                  }}
+                  placeholder={placeholder}
+                  value={
+                    quantityUnit
+                      ? `${quantityUnit.code} ${quantityUnit.name}`
+                      : ""
+                  }
+                />
+              );
+            }}
+          />
+        </FormControl>
+      </div>
+    </div>
+  );
+};
+
+export default Quantity;
