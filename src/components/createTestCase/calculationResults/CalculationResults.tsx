7eb5db9500457af3a18579c178de044b65dc6667
Kaplan <43095@icfrk9y3jjmwk.attlocal.net>
2023-08-23 15:03:19 -0700

    push things around to get scroll bar visible

--- src/components/createTestCase/calculationResults/CalculationResults.tsx
+++ src/components/createTestCase/calculationResults/CalculationResults.tsx
@@ -37,7 +37,7 @@ const CalculationResults = ({
   );
 
   return (
-    <div tw="p-5">
+    <div tw="p-5" style={{ paddingRight: ".25rem" }}>
       {!calculationResults && !calculationErrors && (
         <MadieAlert
           type="info"
