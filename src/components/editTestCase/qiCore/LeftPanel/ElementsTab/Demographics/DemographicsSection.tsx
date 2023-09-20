b10ccfbc37caeebde2d178adc60ca1c00ee4ce4f
Kaplan <43095@icfrk9y3jjmwk.attlocal.net>
2023-09-20 07:30:39 -0700

    removed imports

--- src/components/editTestCase/qiCore/LeftPanel/ElementsTab/Demographics/DemographicsSection.tsx
+++ src/components/editTestCase/qiCore/LeftPanel/ElementsTab/Demographics/DemographicsSection.tsx
@@ -38,12 +38,6 @@ import {
   ResourceActionType,
   useQiCoreResource,
 } from "../../../../../../util/QiCorePatientProvider";
-import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import {
-  birthDateLabelStyle,
-  textFieldStyle,
-} from "./DemographicsSectionStyles";
 import dayjs from "dayjs";
 
 const SELECT_ONE_OPTION = (
@@ -171,8 +165,8 @@ const DemographicsSection = ({ canEdit }) => {
                   label="Date of Birth"
                   disabled={!canEdit}
                   value={patient?.birthDate ? dayjs(patient.birthDate) : null}
-                  handleDateChange={(event: any) => {
-                    const date = new Date(event);
+                  handleDateChange={(value: any) => {
+                    const date = new Date(value);
                     const year = date.getFullYear();
                     const month = String(date.getMonth() + 1).padStart(2, "0"); // Adding 1 because months are zero-indexed
                     const day = String(date.getDate()).padStart(2, "0");
