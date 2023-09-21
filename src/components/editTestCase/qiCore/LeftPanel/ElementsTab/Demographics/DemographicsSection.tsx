349d077291287413d73026f88d5c152d274a0b80
Ethan Kaplan <47760891+ethankaplan@users.noreply.github.com>
2023-09-21 10:34:49 -0700

    Remove comment

--- src/components/editTestCase/qiCore/LeftPanel/ElementsTab/Demographics/DemographicsSection.tsx
+++ src/components/editTestCase/qiCore/LeftPanel/ElementsTab/Demographics/DemographicsSection.tsx
@@ -168,7 +168,7 @@ const DemographicsSection = ({ canEdit }) => {
                   handleDateChange={(value: any) => {
                     const date = new Date(value);
                     const year = date.getFullYear();
-                    const month = String(date.getMonth() + 1).padStart(2, "0"); // Adding 1 because months are zero-indexed
+                    const month = String(date.getMonth() + 1).padStart(2, "0");
                     const day = String(date.getDate()).padStart(2, "0");
                     const formattedDate = `${year}-${month}-${day}`;
                     handleBirthDateChange(formattedDate);
