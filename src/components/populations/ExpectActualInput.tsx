3a212722897b6a52fe63788bd128a5e030497392
mcmcphillips <matt.mcphillips@icf.com>
2023-10-04 14:38:03 -0700

    remove unused code

--- src/components/populations/ExpectActualInput.tsx
+++ src/components/populations/ExpectActualInput.tsx
@@ -12,14 +12,11 @@ const ExpectActualInput = ({
     expected: displayType === "expected",
     actual: displayType === "actual",
   });
-
-  return (props.populationBasis?.toLowerCase() === "boolean" ||
-    props.populationBasis?.toLowerCase() === "true" ||
-    props?.stratificationBasis?.toLowerCase() === "true") &&
-    props.name !== "measureObservation" &&
-    props.name !== "measurePopulationObservation" &&
-    props.name !== "numeratorObservation" &&
-    props.name != "denominatorObservation" ? (
+  return props.populationBasis?.toLowerCase() === "boolean" ||
+    (props.name !== "measureObservation" &&
+      props.name !== "measurePopulationObservation" &&
+      props.name !== "numeratorObservation" &&
+      props.name != "denominatorObservation") ? (
     <input
       type="checkbox"
       className={checkBoxClass}
