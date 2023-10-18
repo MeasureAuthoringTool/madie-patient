a3d07e1367a6e4a7f914921a8c9e96f6403c4407
Prateek Keerthi <prateekkeerthi7@gmail.com>
2023-10-18 08:12:13 -0600

    fixed styling

--- src/components/editTestCase/groupCoverage/GroupCoverageResultsSection.tsx
+++ src/components/editTestCase/groupCoverage/GroupCoverageResultsSection.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import ExpandLessIcon from "@mui/icons-material/ExpandLess";
+import "twin.macro";
+import "styled-components/macro";
 
 const GroupCoverageResultsSection = ({ results }) => {
   const [showGroupCoverageResults, setShowGroupCoverageResults] =
@@ -11,47 +13,54 @@ const GroupCoverageResultsSection = ({ results }) => {
     setShowGroupCoverageResults(!showGroupCoverageResults);
   };
   return (
-    <div>
+    <div tw="flex-auto p-3">
       <button
         onClick={(e) => {
           handleResultsCollapse(e);
         }}
+        style={{
+          width: "fit",
+          marginTop: "10px",
+          position: "relative",
+          float: "right",
+        }}
       >
-        <div style={{ float: "right" }}>
-          {/* <span
-            style={{
-              backgroundColor: "#3498db",
-              color: "#fff",
-              padding: "10px",
-              border: "1px solid  #2980b9",
-              borderRadius: "5px",
-              float: "right",
-              marginLeft: "160px",
-            }}
-          > */}
+        <div
+          style={{
+            fontFamily: "Rubik",
+            fontSize: "14px",
+            fontWeight: "500",
+            color: "#0073C8",
+          }}
+        >
           Results
           {showGroupCoverageResults ? <ExpandLessIcon /> : <ExpandMoreIcon />}
-          {/* </span> */}
-          {showGroupCoverageResults && (
-            <div
-              style={{
-                padding: "10px",
-                marginLeft: "10px",
-                marginRight: "10px",
-                height: "49px",
-                border: "1px solid #EDEDED",
-                backgroundColor: "#EDEDED",
-                fontFamily: "sans-serif",
-                borderRadius: "5px",
-              }}
-              data-testId="results-section"
-              id="results"
-            >
-              {results.trim()}{" "}
-            </div>
-          )}
         </div>
       </button>
+
+      <div>
+        {showGroupCoverageResults && (
+          <div
+            style={{
+              padding: "10px",
+              marginLeft: "10px",
+              marginRight: "10px",
+              marginTop: "50px",
+              width: "auto",
+              height: "75px",
+              border: "1px solid #EDEDED",
+              backgroundColor: "#EDEDED",
+              fontFamily: "sans-serif",
+              borderRadius: "5px",
+              color: "black",
+            }}
+            data-testId="results-section"
+            id="results"
+          >
+            {results.trim()}{" "}
+          </div>
+        )}
+      </div>
     </div>
   );
 };
