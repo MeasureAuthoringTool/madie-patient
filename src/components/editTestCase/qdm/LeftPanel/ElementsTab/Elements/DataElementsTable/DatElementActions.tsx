66d5b0238b59f4f412f67a841e74021513b77396
Kaplan <43095@icfrk9y3jjmwk.attlocal.net>
2023-10-30 07:14:58 -0700

    commit to change branch

--- src/components/editTestCase/qdm/LeftPanel/ElementsTab/Elements/DataElementsTable/DatElementActions.tsx
+++ src/components/editTestCase/qdm/LeftPanel/ElementsTab/Elements/DataElementsTable/DatElementActions.tsx
@@ -63,7 +63,11 @@ export default function DatElementActions(props: DatElementMenuProps) {
             label: "Delete",
             toImplementFunction: deleteDataElement,
             dataTestId: `delete-element-${elementId}`,
-          },
+          },{
+            label: "View",
+            toImplementFunction: viewDataElement,
+            dataTestId: `view-element-${elementId}`,
+          }
         ]}
       />
     </div>
