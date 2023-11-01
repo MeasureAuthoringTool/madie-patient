c5a91ee2f652902eeed9d49024ada903b971d8de
Kaplan <43095@icfrk9y3jjmwk.attlocal.net>
2023-11-01 09:00:58 -0700

    lint

--- src/components/editTestCase/qdm/LeftPanel/ElementsTab/Elements/DataElementsCard/DataElementsCard.tsx
+++ src/components/editTestCase/qdm/LeftPanel/ElementsTab/Elements/DataElementsCard/DataElementsCard.tsx
@@ -107,7 +107,7 @@ const DataElementsCard = (props: {
     setCardActiveTab,
     selectedDataElement,
     setSelectedDataElement,
-    canEdit=false,
+    canEdit = false,
     onChange,
   } = props;
   const [codeSystemMap, setCodeSystemMap] = useState(null);
