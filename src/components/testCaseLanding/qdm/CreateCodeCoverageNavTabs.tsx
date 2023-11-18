6d326c0d78056ea737525a8c4ba6ab7fe4734520
Christopher Hubert <christopher.hubert@semanticbits.com>
2023-11-18 14:53:18 -0500

    fix tests

--- src/components/testCaseLanding/qdm/CreateCodeCoverageNavTabs.tsx
+++ src/components/testCaseLanding/qdm/CreateCodeCoverageNavTabs.tsx
@@ -157,23 +157,25 @@ export default function CreateCodeCoverageNavTabs(props: NavTabProps) {
           />
         </Tabs>
         <div style={{ margin: "6px 0 0 auto", display: "flex" }}>
-          <div>
-            <Button
-              onClick={() => {
-                if (onImportTestCases) {
-                  onImportTestCases();
-                }
-              }}
-              disabled={!canEdit}
-              data-testid="show-import-test-cases-button"
-            >
-              <FileUploadIcon
-                style={{ margin: "0 5px 0 -2px" }}
-                fontSize="small"
-              />
-              Import Test Cases
-            </Button>
-          </div>
+          {featureFlags?.importTestCases && (
+            <div>
+              <Button
+                onClick={() => {
+                  if (onImportTestCases) {
+                    onImportTestCases();
+                  }
+                }}
+                disabled={!canEdit}
+                data-testid="show-import-test-cases-button"
+              >
+                <FileUploadIcon
+                  style={{ margin: "0 5px 0 -2px" }}
+                  fontSize="small"
+                />
+                Import Test Cases
+              </Button>
+            </div>
+          )}
           <div style={{ margin: "0 6px 0 26px" }}>
             <Button
               disabled={!canEdit}
