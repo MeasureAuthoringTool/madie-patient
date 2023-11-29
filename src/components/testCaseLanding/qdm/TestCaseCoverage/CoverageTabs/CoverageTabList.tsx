21f7dc5830f24c9c3f2240bf0345030f159df0b7
Ethan Kaplan <ethan.kaplan@semanticbits.com>
2023-11-29 13:39:40 -0800

    remove unneeded imports

--- src/components/testCaseLanding/qdm/TestCaseCoverage/CoverageTabs/CoverageTabList.tsx
+++ src/components/testCaseLanding/qdm/TestCaseCoverage/CoverageTabs/CoverageTabList.tsx
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from "react";
-import _ from "lodash";
+import React from "react";
 import CoverageTab from "./CoverageTab";
 
 import { MappedCql } from "../../../../../util/GroupCoverageHelpers";
