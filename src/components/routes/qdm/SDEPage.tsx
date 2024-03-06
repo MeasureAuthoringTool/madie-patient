481170e599eb0deb5958dfcbdd44ca72c9790ef1
Christopher Hubert <christopher.hubert@semanticbits.com>
2024-03-06 14:10:36 -0500

    MAT-6660 update formik call

--- src/components/routes/qdm/SDEPage.tsx
+++ src/components/routes/qdm/SDEPage.tsx
@@ -106,10 +106,7 @@ const SDEPage = () => {
   return (
     <form
       id="sde-form"
-      onSubmit={(e) => {
-        e.preventDefault();
-        handleSubmit();
-      }}
+      onSubmit={formik.handleSubmit}
       data-testid={`sde-form`}
       style={{ minHeight: 539 }}
     >
