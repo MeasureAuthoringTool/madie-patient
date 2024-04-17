852554ef8a77294a3f87884d4fb7041f90278582
Rohit Kandimalla <rohit.kandimalla@icf.com>
2024-04-17 11:51:12 -0400

    MAT-6729 v2 removed unused imports

--- src/api/useExcelExportService.test.ts
+++ src/api/useExcelExportService.test.ts
@@ -1,13 +1,5 @@
 import axios from "axios";
-import useExcelExportService, {
-  ExcelExportService,
-} from "./useExcelExportService";
-
-import { cqm_measure_basic_valueset } from "../mockdata/qdm/CMS108/cqm_measure_basic_valueset";
-
-import * as _ from "lodash";
-import { Simulate } from "react-dom/test-utils";
-import error = Simulate.error;
+import { ExcelExportService } from "./useExcelExportService";
 
 jest.mock("axios");
 
@@ -21,7 +13,7 @@ describe("useExcelExport Tests", () => {
   it("Should call Terminology Service URL to fetch value set expansions when manifest Expansion feature flag is true", async () => {
     axios.put = jest.fn().mockResolvedValue({ data: "test-data" });
 
-    var result = await excelExportService.generateExcel([]);
+    const result = await excelExportService.generateExcel([]);
     expect(result.data).toEqual("test-data");
   });
 });
