90323a6d79e8349c17608be4b4e2ebf2599ad194
mcmcphillips <matt.mcphillips@icf.com>
2024-06-27 22:23:22 -0700

    MAT-7341: Hit the low hanging edge cases

--- src/components/editTestCase/qdm/populations/TestCasePopulation.test.tsx
+++ src/components/editTestCase/qdm/populations/TestCasePopulation.test.tsx
@@ -46,6 +46,83 @@ describe("TestCasePopulation component", () => {
     expect(buttons).toHaveLength(2);
   });
 
+  it("should render test case population strat", async () => {
+    const testCasePopulation = {
+      id: "1234",
+      name: PopulationType.INITIAL_POPULATION,
+      expected: true,
+      actual: true,
+    };
+    const handleChange = jest.fn();
+    render(
+      <MemoryRouter>
+        <table>
+          <tbody>
+            <TestCasePopulation
+              isTestCaseExecuted
+              population={testCasePopulation}
+              populationResult={testCasePopulation}
+              strat
+              onChange={handleChange}
+              populationBasis="true"
+              error={error}
+              i={0}
+              measureObservationsCount={0}
+              initialPopulationCount={1}
+            />
+          </tbody>
+        </table>
+      </MemoryRouter>
+    );
+    const row = screen.getByTestId(
+      `strat-test-row-population-id-${testCasePopulation.name}-0`
+    );
+    expect(row).toBeInTheDocument();
+    const columns = row.querySelectorAll("td");
+    expect(columns[1]).toHaveTextContent("Initial Population");
+    const buttons = await screen.findAllByRole("checkbox");
+    expect(buttons).toHaveLength(2);
+  });
+
+  it("should render test case population strat with error", async () => {
+    const testCasePopulation = {
+      id: "1234",
+      name: PopulationType.INITIAL_POPULATION,
+      expected: true,
+      actual: true,
+    };
+    const handleChange = jest.fn();
+    render(
+      <MemoryRouter>
+        <table>
+          <tbody>
+            <TestCasePopulation
+              isTestCaseExecuted
+              population={testCasePopulation}
+              populationResult={testCasePopulation}
+              strat
+              onChange={handleChange}
+              populationBasis="true"
+              error={{ expected: "expected?" }}
+              i={0}
+              measureObservationsCount={0}
+              initialPopulationCount={1}
+            />
+          </tbody>
+        </table>
+      </MemoryRouter>
+    );
+    const row = screen.getByTestId(
+      `strat-test-row-population-id-${testCasePopulation.name}-0`
+    );
+    expect(row).toBeInTheDocument();
+    expect(
+      screen.getByTestId("initialPopulation-error-helper-text")
+    ).toBeInTheDocument();
+    // const buttons = await screen.findAllByRole("checkbox");
+    // expect(buttons).toHaveLength(2);
+  });
+
   it("should handle changes to checkboxes", async () => {
     const testCasePopulation = {
       id: "1234",
