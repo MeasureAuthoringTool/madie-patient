509e817edf8b656fbc82f09eed8505944a09c1e6
Prateek Keerthi <prateekkeerthi7@gmail.com>
2024-08-07 12:59:02 -0400

    MAT-7508 fixed test case

--- src/components/createTestCase/__mocks__/testCaseFixture.ts
+++ src/components/createTestCase/__mocks__/testCaseFixture.ts
@@ -6,7 +6,7 @@ export const testCaseFixture: TestCase = {
   executionStatus: "false",
   name: "Initial Population",
   title: "Encounter",
-  series: "DENOM_Pass",
+  series: "DENOMPass",
   description: "Encounter start within MP.",
   createdAt: "2022-03-24T17:40:15.921Z",
   createdBy: "joseph.kotanchik@semanticbits.com",
