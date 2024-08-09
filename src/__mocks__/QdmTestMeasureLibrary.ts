2415406ee92d7512f5176899243bb1c33ac436dc
Ethan Kaplan <ethan.kaplan@semanticbits.com>
2024-08-09 13:15:59 -0700

    MAT-7527 fix test

--- src/__mocks__/QdmTestMeasureLibrary.ts
+++ src/__mocks__/QdmTestMeasureLibrary.ts
@@ -2795,6 +2795,6 @@ export const testMeasureCalculationResult = {
 export const ipHighlighting = `<pre style="tab-size: 2;"
   data-library-name="QDMTestMeasure" data-statement-name="Initial Population">
 <code>
-<span data-ref-id="18" style="color:#4D7E23;border-bottom-color:#4D7E23;border-bottom-width:3px"><span>define &quot;Initial Population&quot;:
-  </span><span data-ref-id="17" style="color:#4D7E23;border-bottom-color:#4D7E23;border-bottom-width:3px"><span>&quot;Qualifying Encounters&quot;</span></span></span></code>
+<span data-ref-id="18" style="color:#4D7E23;border-bottom-color:#4D7E23;border-bottom-width:3px;background-color:#ccebe0"><span>define &quot;Initial Population&quot;:
+</span><span data-ref-id="17" style="color:#4D7E23;border-bottom-color:#4D7E23;border-bottom-width:3px;background-color:#ccebe0"><span>&quot;Qualifying Encounters&quot;</span></span></span></code>
 </pre>`;
