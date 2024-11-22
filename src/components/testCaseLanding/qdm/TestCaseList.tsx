5f31fb8e2bdaffdd08ad892178367f70199db066
adongare <ashok.dongare@semanticbits.com>
2024-11-22 10:32:39 -0500

    MAT-7936 reset the status for new measure

--- src/components/testCaseLanding/qdm/TestCaseList.tsx
+++ src/components/testCaseLanding/qdm/TestCaseList.tsx
@@ -175,6 +175,7 @@ const TestCaseList = (props: TestCaseListProps) => {
   useState<GroupCoverageResult>();
   const [createOpen, setCreateOpen] = useState<boolean>(false);
   useEffect(() => {
+    setExecuteAllTestCases(false);
     if (
       !_.isNil(measure?.groups) &&
       measure.groups.length > 0 &&
@@ -274,8 +275,8 @@ const TestCaseList = (props: TestCaseListProps) => {
         passFailRatio: passFailRatio,
       });
       setTestCases([...testCases]);
+      setCoveragePercentage(clauseCoverageProcessor());
     }
-    setCoveragePercentage(clauseCoverageProcessor());
   }, [calculationOutput, selectedPopCriteria]);
 
   const clauseCoverageProcessor = (measureGroup?: Group): string => {
